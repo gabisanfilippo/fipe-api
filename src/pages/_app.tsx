362a2a8bcd522d1998f7a308f5c997ef24b4577b
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-02-25 00:44:54 -0300

    feat: react-query provider configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { GlobalStyle } from '@styles/index';
 import type { AppProps } from 'next/app'
+import { useRef } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = useRef(new QueryClient());
   return (
-    <>
+    <QueryClientProvider client={queryClient.current}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   );
 }
