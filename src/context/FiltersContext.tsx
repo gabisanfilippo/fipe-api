91a9f5fbb0d493051b00f2ae80629b39ff20052b
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-02-25 19:08:42 -0300

    feat: getServerSidePropsFunction

--- src/context/FiltersContext.tsx
+++ src/context/FiltersContext.tsx
@@ -1,6 +1,10 @@
-import { createContext, FC, useState } from "react";
+import { API } from "@services/api";
+import { createContext, FC, useEffect, useState } from "react";
+import { dehydrate, QueryClient } from "react-query";
 
 export const FiltersContext = createContext<IContext | null>(null);
+
+let filtersGlobal: IFilters;
 
 export const FiltersContextProvider: FC<IProvider> = ({ children }) => {
   const [filters, setFilters] = useState<IFilters>({
@@ -13,6 +17,11 @@ export const FiltersContextProvider: FC<IProvider> = ({ children }) => {
     models: [{ label: "Modelos", value: " " }],
     years: [{ label: "Anos", value: " " }],
   });
+
+  useEffect(() => {
+    filtersGlobal = filters;
+  }, [filters]);
+
   return (
     <FiltersContext.Provider
       value={{ filters, setFilters, options, setOptions }}
@@ -21,3 +30,31 @@ export const FiltersContextProvider: FC<IProvider> = ({ children }) => {
     </FiltersContext.Provider>
   );
 };
+
+export async function getServerSideProps() {
+  const filters = filtersGlobal;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(["getModels", filters.brands], async () => {
+    const { data } = await API.get(
+      `/fipe/api/v1/carros/marcas/${filters.brands}/modelos`
+    );
+    return data;
+  });
+  await queryClient.prefetchQuery(["getYears", filters], async () => {
+    const { data } = await API.get(
+      `/fipe/api/v1/carros/marcas/${filters.brands}/modelos/${filters.models}/anos`
+    );
+    return data;
+  });
+  await queryClient.prefetchQuery(["getPrices", filters], async () => {
+    const { data } = await API.get(
+      `/fipe/api/v1/carros/marcas/${filters.brands}/modelos/${filters.models}/anos/${filters.years}`
+    );
+    return data;
+  });
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
